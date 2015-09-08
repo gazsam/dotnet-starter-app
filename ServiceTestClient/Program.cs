ed5ac775442cda99596a390cb57593165517a999
Sam Gazitt <gazitt@gmail.com>
2015-09-08 10:56:24 -0700

    first commit

---
+++ ServiceTestClient/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceTestClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Creating proxy");
+            var client = new WorkerClient();
+            Console.WriteLine("Calling method on proxy");
+            client.DoWork();
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ dotnet-starter-app/IWorker.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace dotnet_starter_app
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IWorker" in both code and config file together.
+    [ServiceContract]
+    public interface IWorker
+    {
+        [OperationContract]
+        void DoWork();
+    }
+}
