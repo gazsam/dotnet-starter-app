0c401a4eeaa33f0dfb16e2c5a5f410986c9d8415
Sam Gazitt <gazitt@gmail.com>
2015-09-10 11:48:39 -0700

    manifest and some service things

--- dotnet-starter-app/App_Start/BundleConfig.cs
+++ dotnet-starter-app/App_Start/BundleConfig.cs
@@ -23,6 +23,9 @@ namespace dotnet_starter_app
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
+           
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+            "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                         "~/Content/bootstrap.css",
--- dotnet-starter-app/Controllers/HomeController.cs
+++ dotnet-starter-app/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Newtonsoft.Json.Linq;
 
 namespace dotnet_starter_app.Controllers
 {
@@ -20,6 +21,14 @@ namespace dotnet_starter_app.Controllers
 
         public ActionResult AddService()
         {
+            //Ask for environment variables
+            string vcap = Environment.GetEnvironmentVariable("VCAP_SERVICES");
+            //If we're on localhost, this will be null
+            if (vcap != null)
+            {
+                JObject services = Newtonsoft.Json.Linq.JObject.Parse(vcap);
+                ViewBag.json = services;
+            }
             return View();
         }
 
